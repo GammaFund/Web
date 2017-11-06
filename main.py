bcff5aba148f41d25bd809f9a278c52152679161
William CW Lau <contact@cwlau.com>
2017-11-06 23:36:04 +0800

    Base web framework setup

--- main.py
+++ main.py
@@ -1,10 +1,34 @@
 #!/usr/bin/env python
+import os
 import webapp2
+
+from google.appengine.ext.webapp import template
+from webapp2_extras import routes
+
+
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-        self.response.write('Coming soon!')
+        # self.response.write('Coming soon!')
+
+        path = os.path.join(os.path.dirname(__file__), 'template/coming_soon.html')
+        params = {}
+        self.response.out.write(template.render(path, params))
+
+
+class PreviewHandler(webapp2.RequestHandler):
+    def get(self):
+
+        path = os.path.join(os.path.dirname(__file__), 'template/index.html')
+        params = {}
+        self.response.out.write(template.render(path, params))
+
+
 
 app = webapp2.WSGIApplication([
-    ('/', MainHandler)
+    routes.DomainRoute(r'<:(gammafund.org|localhost)>', [
+        webapp2.Route('/', MainHandler),
+    ]),
+
+    ('/', PreviewHandler)
 ])
